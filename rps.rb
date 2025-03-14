d26f2db4ec45524e86965c0cb06418052a963508
tomball357 <tball@chicagobooth.edu>
2025-03-14 04:04:58 +0000

    fina

--- rps.rb
+++ rps.rb
@@ -3,70 +3,48 @@ require "sinatra/reloader"
 require "better_errors"
 require "binding_of_caller"
 
-# Need this configuration for better_errors
+# Configuration for better_errors
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
+# Helper function for game logic
+def rps_game(player_choice)
+  choices = ["rock", "paper", "scissors"]
+  computer_choice = choices.sample  # Random selection
 
+  if player_choice == computer_choice
+    result = "We tied!"
+  elsif (player_choice == "rock" && computer_choice == "scissors") ||
+        (player_choice == "paper" && computer_choice == "rock") ||
+        (player_choice == "scissors" && computer_choice == "paper")
+    result = "We won!"
+  else
+    result = "We lost!"
+  end
+
+  @outcome = "We played #{player_choice}, they played #{computer_choice}! #{result}"
+end
+
+# Homepage with game rules
 get("/") do
-  erb(:home)
+  erb :rules
 end
 
+# Rock route
 get("/rock") do
-  choice = ["rock", "paper", "scissors"]
-  plych = "rock"
-  cch = choice.sample
-	
-  if plych == cch
-    final = "It's a tie"
-  elsif (plych == "rock" && cch == "scissors") ||
-        (plych == "paper" && cch == "rock") ||
-        (plych == "scissors" && cch == "paper")
-    final = "We won"
-  else
-    final = "We lost"
-  end
-  
-
-  @outcome = "We chose #{plych}! They chose #{cch}! #{final}!"
-  erb(:rock)
+  @player_choice, @computer_choice, @result = rps_game("rock")
+  erb :rock
 end
 
+# Paper route
 get("/paper") do
-  choice = ["rock", "paper", "scissors"]
-  plych = "paper"
-  cch = choice.sample
-	
-  if plych == cch
-    final = "It's a tie!"
-  elsif (plych == "rock" && cch == "scissors") ||
-        (plych == "paper" && cch == "rock") ||
-        (plych == "scissors" && cch == "paper")
-    final = "We won!"
-  else
-    final = "We lost!"
-  end
-
-  @outcome = "We chose #{plych}! They chose #{cch}! #{final}!"
-  erb(:paper)
+  @player_choice, @computer_choice, @result = rps_game("paper")
+  erb :paper
 end
 
+# Scissors route
 get("/scissors") do
-  choice = ["rock", "paper", "scissors"]
-  plych = "scissors"
-  cch = choice.sample
-	
-  if plych == cch
-    final = "It's a tie!"
-  elsif (plych == "rock" && cch == "scissors") ||
-        (plych == "paper" && cch == "rock") ||
-        (plych == "scissors" && cch == "paper")
-    final = "We won!"
-  else
-    final = "We lost!"
-  end
-
-  @outcome = "We chose #{plych}! They chose #{cch}! #{final}!"
-  erb(:scissors)
+  @player_choice, @computer_choice, @result = rps_game("scissors")
+  erb :scissors
 end
